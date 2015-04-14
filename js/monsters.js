919e642a11336c0918419523967f2e044ba9ceb0
José Papaianni <jose.papaianni@fdvsolutions.com>
2015-04-14 00:07:48 -0300

    Added some UI events

--- js/monsters.js
+++ js/monsters.js
@@ -1,7 +1,6 @@
 var levelState = {
     preload: function () {
         game.load.image("backgroundGlobal", "assets/back-pattern.jpg", 150, 150);
-		game.load.image("selector", "assets/selector-icon.png", 110, 110);
         
         for (var i = 0; i<cellTypes.length; i++){
             game.load.spritesheet(cellTypes[i].name, cellTypes[i].filename , cellTypes[i].w,cellTypes[i].h);
@@ -18,7 +17,7 @@ var levelState = {
                     backgroundGlobal = game.add.image(xPos, j * 150, 'backgroundGlobal');
             }
         }
-        
+        selectedCells = [];
         levelPath = levelsConfig[currentLevel].path;
 		startPosition = levelsConfig[currentLevel].startPosition;
         
@@ -58,7 +57,7 @@ function addCellMovement(cell){
     setInterval( function () {            
         if (cell.currentStep < (levelPath.length-1)) {
           //  var nextCell = getNextCell(levelPath[cell.currentStep],levelPath[cell.currentStep+1]);
-            TweenMax.to(cell, 0.25,{
+            TweenMax.to(cell, 0.5,{
                 x: getCellPosition(cell.currentStep+1).positionX,
                 y: getCellPosition(cell.currentStep+1).positionY,
                 ease: Back.easeInOut.config(1),
@@ -80,50 +79,60 @@ function getCellDistance(selection,target){
 	return {offsetX: Math.abs(((selection.x-target.x))), offsetY: Math.abs(((selection.y-target.y)))}
 };
 
-
-var selectedCells = [];
-
-
 function swapCellPosition (cell){	
 	if (allowSelectCell && selectedCells.length <2){
+        clearInterval(cell.reflexInterval);
 		selectedCells.push (cell);
-		cell.frame = 9;
-	 	if ( allowSelectCell && selectedCells.length === 2 ){
+        cell.frame = 9;
+        if ( allowSelectCell && selectedCells.length === 2 ){
 			var currentCell = selectedCells[0];
 			var targetCell = selectedCells[1];
+            var currentS = currentCell.currentStep;
+            var targetS = targetCell.currentStep;
 			cellDistance = getCellDistance (currentCell,targetCell);
 			if (cellDistance.offsetX <= 66 && cellDistance.offsetY <= 66 !=
 				(cellDistance.offsetX == 66 && cellDistance.offsetY == 66)){
-				var currentS = currentCell.currentStep;
-				var targetS = targetCell.currentStep;
+				
 				TweenMax.to(currentCell, 1,{
 							x: getCellPosition(targetS).positionX,
 							y: getCellPosition(targetS).positionY,
-							ease: Power3.easeOut
+							ease: Power3.easeOut,
+                            onComplete: currentCell.frame = 0
 						});
 				TweenMax.to(targetCell, 1,{
 							x: getCellPosition(currentS).positionX,
 							y: getCellPosition(currentS).positionY,
-							ease: Power3.easeOut
+							ease: Power3.easeOut,
+                            onComplete: targetCell.frame = 0
 						});
 				currentCell.currentStep = targetS;
 				targetCell.currentStep = currentS;
 				
-			};
-			
+			} else if (allowSelectCell){
+                currentCell.frame = 0;
+                var currentCellPosX = getCellPosition(currentS).positionX 
+                currentCell.x = currentCellPosX -4;
+                TweenMax.to (currentCell, 0.05, {
+                    x: currentCellPosX + 8,
+                    yoyo: true,
+                    repeat: 10,
+                    onComplete: function () { currentCell.x = currentCellPosX }
+                })
+            }
+			addCellEffects(cell);
+            cell.frame = 0;
 			selectedCells = []
-		};
+		} 
 	}
 }
 	
 
 //Effects
 function addCellEffects (cell){
-	cell.animations.add('reflex',[1,2,3,4,5,6,7,8,9,0], 60, false);
-	
-	setInterval(function(){
+	cell.animations.add('reflex',[1,2,3,4,5,6,7,8,0], 60, false);
+	cell.reflexInterval = setInterval(function(){
 			cell.animations.play('reflex', 45, false);
-	},3500);
+	},2500);
 };
 
 
